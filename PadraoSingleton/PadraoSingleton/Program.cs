86fb777b389ec2b9f588c901e87986d773de5c08
Elayne Argollo <elayne.natalia@outlook.com>
2021-08-05 20:12:50 -0300

    singleton

---
+++ PadraoSingleton/PadraoSingleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PadraoSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contador = 2;
+            
+            while(contador >= 0)
+            {
+                Console.WriteLine(Singleton.Instance.Id);
+                contador--;
+            }
+        }
+    }
+}
